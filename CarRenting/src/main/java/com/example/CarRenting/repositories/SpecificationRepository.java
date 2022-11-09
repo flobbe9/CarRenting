25f9f20dcf0ac20d2386193e4e27f2f0ee00bf5b
flobbe9 <florin735@live.com>
2022-11-09 13:21:02 +0100

    feat: add specification getter in service and repo

--- CarRenting/src/main/java/com/example/CarRenting/repositories/SpecificationRepository.java
+++ CarRenting/src/main/java/com/example/CarRenting/repositories/SpecificationRepository.java
@@ -1,5 +1,7 @@
 package com.example.CarRenting.repositories;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -9,4 +11,10 @@ import com.example.CarRenting.models.Specification;
 @Repository
 public interface SpecificationRepository extends JpaRepository<Specification, Long> {
     
+    Optional<Specification> findByNumSeatsAndNumDoorsAndHpAndSpeedMaxAndWeightUnladenAndWeightMax(int numSeats,
+                                                                                                  int numDoors,
+                                                                                                  int hp, 
+                                                                                                  int speedMax,
+                                                                                                  double weightUnladen,
+                                                                                                  double weightMax);
 }
--- CarRenting/src/main/java/com/example/CarRenting/services/SpecificationService.java
+++ CarRenting/src/main/java/com/example/CarRenting/services/SpecificationService.java
@@ -13,4 +13,28 @@ public class SpecificationService {
     @Autowired
     private SpecificationRepository specificationRepository;
 
+
+    public Specification getSpecification(int numSeats,
+                                          int numDoors, 
+                                          int hp,
+                                          int speedMax,
+                                          double weightUnladen,
+                                          double weightMax) {
+
+        return specificationRepository
+                .findByNumSeatsAndNumDoorsAndHpAndSpeedMaxAndWeightUnladenAndWeightMax(numSeats, 
+                                                                                       numDoors, 
+                                                                                       hp, 
+                                                                                       speedMax, 
+                                                                                       weightUnladen, 
+                                                                                       weightMax)
+                .orElseThrow(() -> 
+                    new IllegalStateException("Could not find a specification with these attributes."));
+    }
+
+
+    public Specification getById(long id) {
+        return specificationRepository.findById(id).orElseThrow(() -> 
+            new IllegalStateException("Could not find a specification with id " + id + "."));
+    }
 }
