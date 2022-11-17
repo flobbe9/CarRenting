ec721a11139f2dbf24da0418e3ed2a18373aebe6
flobbe9 <florin735@live.com>
2022-11-17 15:36:15 +0100

    feat: populate db on api start with data.sql

---
+++ Spring-Boot/src/main/resources/data.sql
@@ -0,0 +1,14 @@
+-- Active: 1667982561949@@127.0.0.1@3306@carrenting
+
+INSERT INTO car(id, brand, model, color, fuel_type, is_available) VALUES(1, "Vokswagen", "Caddy", "RED", "BENZINE", true);
+INSERT INTO car(id, brand, model, color, fuel_type, is_available) VALUES(2, "Vokswagen", "Golf VIII", "YELLOW", "DIESEL", true);
+INSERT INTO car(id, brand, model, color, fuel_type, is_available) VALUES(3, "Audi", "A1 Sportback", "SILVER", "BENZINE", true);
+INSERT INTO car(id, brand, model, color, fuel_type, is_available) VALUES(4, "Tesla", "Model X", "WHITE", "ELECTRIC", true);
+INSERT INTO car(id, brand, model, color, fuel_type, is_available) VALUES(5, "Lamborghini", "Aventador LP 780-4 Roadster", "BLUE", "BENZINE", true);
+
+
+INSERT INTO specification(id, num_seats, num_doors, hp, speed_max, weight_unladen, weight_max) VALUES(1, 5, 5, 122, 186, 1608, 2250);
+INSERT INTO specification(id, num_seats, num_doors, hp, speed_max, weight_unladen, weight_max) VALUES(2, 5, 5, 245, 250, 1388, 1950);
+INSERT INTO specification(id, num_seats, num_doors, hp, speed_max, weight_unladen, weight_max) VALUES(3, 5, 5, 150, 217, 1170, 1710);
+INSERT INTO specification(id, num_seats, num_doors, hp, speed_max, weight_unladen, weight_max) VALUES(4, 7, 5, 611, 250, 2562, 3120);
+INSERT INTO specification(id, num_seats, num_doors, hp, speed_max, weight_unladen, weight_max) VALUES(5, 2, 2, 780, 355, 1600, 2100);
