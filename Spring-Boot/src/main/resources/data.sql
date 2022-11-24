d7dd9cbccde2548069270d276a3356d80039c55a
flobbe10 <florin.schikarski@tecfox.eu>
2022-11-24 11:54:24 +0100

    fix: rename horsePower to horse_power in sql

--- Spring-Boot/src/main/resources/data.sql
+++ Spring-Boot/src/main/resources/data.sql
@@ -7,8 +7,8 @@ INSERT INTO car(id, brand, model, color, fuel_type, specifiaction_id, is_availab
 INSERT INTO car(id, brand, model, color, fuel_type, specifiaction_id, is_available) VALUES(5, "Lamborghini", "Aventador LP 780-4 Roadster", "BLUE", "BENZINE", 5, true);
 
 
-INSERT INTO specification(id, num_seats, num_doors, horsePower, speed_max, weight_unladen, weight_max, car_id) VALUES(1, 5, 5, 122, 186, 1608, 2250, 1);
-INSERT INTO specification(id, num_seats, num_doors, horsePower, speed_max, weight_unladen, weight_max, car_id) VALUES(2, 5, 5, 245, 250, 1388, 1950, 2);
-INSERT INTO specification(id, num_seats, num_doors, horsePower, speed_max, weight_unladen, weight_max, car_id) VALUES(3, 5, 5, 150, 217, 1170, 1710, 3);
-INSERT INTO specification(id, num_seats, num_doors, horsePower, speed_max, weight_unladen, weight_max, car_id) VALUES(4, 7, 5, 611, 250, 2562, 3120, 4);
-INSERT INTO specification(id, num_seats, num_doors, horsePower, speed_max, weight_unladen, weight_max, car_id) VALUES(5, 2, 2, 780, 355, 1600, 2100, 5);
+INSERT INTO specification(id, num_seats, num_doors, horse_power, speed_max, weight_unladen, weight_max, car_id) VALUES(1, 5, 5, 122, 186, 1608, 2250, 1);
+INSERT INTO specification(id, num_seats, num_doors, horse_power, speed_max, weight_unladen, weight_max, car_id) VALUES(2, 5, 5, 245, 250, 1388, 1950, 2);
+INSERT INTO specification(id, num_seats, num_doors, horse_power, speed_max, weight_unladen, weight_max, car_id) VALUES(3, 5, 5, 150, 217, 1170, 1710, 3);
+INSERT INTO specification(id, num_seats, num_doors, horse_power, speed_max, weight_unladen, weight_max, car_id) VALUES(4, 7, 5, 611, 250, 2562, 3120, 4);
+INSERT INTO specification(id, num_seats, num_doors, horse_power, speed_max, weight_unladen, weight_max, car_id) VALUES(5, 2, 2, 780, 355, 1600, 2100, 5);
